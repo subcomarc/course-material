22a4b1003496c210dcee25c8c99f6a46c9cef91b
Hernan Anllo <subcomarc@gmail.com>
2014-09-22 17:29:58 +0200

    just a commit

---
+++ exercices/030/solution.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+for i in range(100):
+    if int(i)%2==0:
+       print(i)
---
+++ exercices/040/solution.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+EndResult=0
+for i in range(101):
+    if int(i)%2==0:
+       EndResult=EndResult+int(i) 
+print(EndResult)

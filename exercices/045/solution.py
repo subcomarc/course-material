0d2e33b79c9e2a7f03ffc8a8e6d04a84f81388d7
Hernan Anllo <subcomarc@gmail.com>
2014-09-22 17:56:00 +0200

    just a commit

---
+++ exercices/045/solution.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+def sqrt(n):
+    print(int(n)**0.5)
---
+++ exercices/060/solution.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+Alphabet='abcdefghijklmnopqrstuvwxyz'
+for i in range(len(Alphabet)):
+    for j in range(len(Alphabet)):
+        print(Alphabet[i]+Alphabet[j])

d5c4e26a8b70ba92b67cdd89ef2180b400afdf5f
Hernan Anllo <subcomarc@gmail.com>
2014-09-22 19:25:52 +0200

    just a commit

---
+++ exercices/200/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+def is_prime(n):
+    Prime=1;
+    TargNum=1;
+
+    for i in range(2,n-1):
+        if int(n)%int(i)==0:
+            TargNum=0
+    print(Prime==TargNum)
+        

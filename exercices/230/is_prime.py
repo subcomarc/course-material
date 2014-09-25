fc989447bc35a080a3d49b0b01be1897618a5380
Hernan Anllo <subcomarc@gmail.com>
2014-09-25 16:11:43 +0200

    just a commit

--- exercices/230/is_prime.py
+++ exercices/230/is_prime.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
+import math
+
+
 def is_prime(n):
     Prime = 1
     TargNum = 1
-    for i in range(2, n-1, 2):
-        if n % i == 0:
+    SecNum = math.ceil(math.sqrt(n))
+    if n % 2 != 0:
+        if n % 3 != 0:
+            if n % 5 != 0:
+                for i in range(7, SecNum+1):
+                    if n % i == 0:
+                        TargNum = 0
+            else:
+                TargNum = 0
+        else:
             TargNum = 0
-#    print(Prime==TargNum)
-    if Prime == TargNum:
-        return n
+    else:
+        TargNum = 0
+    if n == 1:
+        return True
+    elif n == 2:
+        return True
+    elif n == 3:
+        return True
+    elif n == 5:
+        return True
+    else:
+        return TargNum == Prime

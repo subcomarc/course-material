9aee667573938e2ac6112a2d98a25ba14d0ed80f
Hernan Anllo <subcomarc@gmail.com>
2014-09-27 20:24:59 +0200

    just a commit

--- exercices/331/solution.py
+++ exercices/331/solution.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
-digits = open('digit')
-TargCiph = []
-BestCiph = []
-Rounds = 0
-Result = 0
-TempRes = 0
-for line in digits:
-    for d in range(len(line)-12):
-        for p in range(13):
-            TargCiph.append(line[d + p])
-        Result = 1
-        TargCiph = [int(h) for h in TargCiph]
-        for i in TargCiph:
-            Result = Result*i
-            if Result > TempRes:
-                TempRes = Result
-                BestCiph = TargCiph
-        TargCiph = []
-print(TempRes)
-digits.close()
+import json
+import re
+MyListofKeys = []
+TheVelibFile = open('velib.json')
+MyVelibFile = json.load(TheVelibFile)
+for corr in MyVelibFile:
+    FutureZip = re.search('\d+', corr['address'])
+    FutureName = re.search('\d+', corr['name'])
+    corr['zip_code'] = FutureZip.group(0)
+    corr['city'] = corr['address'][FutureZip.start()+6:]
+    corr['address'] = corr['address'][:FutureZip.start()-3]
+    corr['name'] = corr['name'][FutureName.end()+3:]
+with open('solution.json', 'w') as output:
+    json.dump(MyVelibFile, output)

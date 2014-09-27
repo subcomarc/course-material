0c2c571f5bce8d72192ac59d9463397cfedc161a
Hernan Anllo <subcomarc@gmail.com>
2014-09-27 17:35:50 +0200

    just a commit

---
+++ exercices/328/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+def mul(n):
+    Result = 1
+    for i in n:
+        Result = Result*i
+    return Result
---
+++ exercices/331/solution.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+digits = open('digit')
+TargCiph = []
+BestCiph = []
+Rounds = 0
+Result = 0
+TempRes = 0
+for line in digits:
+    for d in range(len(line)-12):
+        for p in range(13):
+            TargCiph.append(line[d + p])
+        Result = 1
+        TargCiph = [int(h) for h in TargCiph]
+        for i in TargCiph:
+            Result = Result*i
+            if Result > TempRes:
+                TempRes = Result
+                BestCiph = TargCiph
+        TargCiph = []
+print(BestCiph)
+digits.close()

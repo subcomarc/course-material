c2bda2630a06364dd9608882bf58c3d541b8924c
Hernan Anllo <subcomarc@gmail.com>
2014-09-28 23:00:53 +0200

    I honestly don't have enough time to go through the whole thing :(

---
+++ exercices/102/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+def check_my_city(city_name):
+    if ...
+        return "Sorry! No station for your city has been found!"
+    else:
+        ...
+        #here it's obviously an example to give you an idea!
+        return {"stations_nb": 2, 
+                "zip_code": ['93170', '93320'],
+                "city": 'pantin'}
+

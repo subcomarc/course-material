96e72b4132d6c85e027858eb3ac9b752dda4c72f
Hernan Anllo <subcomarc@gmail.com>
2014-09-28 22:20:25 +0200

    yuppppp

--- exercices/202/solution.py
+++ exercices/202/solution.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 def starts_with(A, B):
     Letter = 1
-    TargChar = 0
-    if A[0] == B[0]:
-        TargChar = 1
+    TargChar = 1
+    if len(A) > len(B):
+        return False
+    for i in range(len(A)):
+        if A[i] != B[i]:
+            TargChar = 0
     return (Letter == TargChar)

2f12da20d50dca638d138a6d9c4e6d5bd910d9bc
Hernan Anllo <subcomarc@gmail.com>
2014-09-28 17:12:47 +0200

    yuppppp

--- exercices/201/solution.py
+++ exercices/201/solution.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 def is_alpha(n):
     import string
-    Letter = 1
     TargChar = 0
 
     for i in range(len(n)):
+        TargChar = 0
         for j in range(len(string.ascii_letters)):
                 if n[i] == string.ascii_letters[j]:
                     TargChar = 1
-    print(Letter == TargChar)
+        if TargChar == 0:
+            return False
+    if TargChar == 1:
+        return True

1dacab8258235f89e6c3aef5283cb888ebbaaac3
Hernan Anllo <subcomarc@gmail.com>
2014-09-28 18:24:29 +0200

    yuppppp

--- exercices/220/solution.py
+++ exercices/220/solution.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-import is_prime   
-Primes=[]
-for i in range(10000,10050): 
+import is_prime
+Primes = []
+for i in range(10000, 10050):
     if is_prime.is_prime(i):
-        Primes=Primes+[i]
+        Primes = Primes + [i]
 print(Primes)
-    
